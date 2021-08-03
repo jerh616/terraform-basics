d28333bf5d1ded9d721ab70ba7025dcbf3ac0dde
JERH O'CONNOR <JEROCONO@ie.ibm.com>
2021-08-03 18:42:48 +0100

    Add LoadBalancer related resources and output

--- main.tf
+++ main.tf
@@ -2,6 +2,8 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+## RESOURCES
+
 resource "aws_launch_configuration" "example" {
   image_id      = "ami-0943382e114f188e8"
   instance_type = "t2.micro"
@@ -35,6 +37,9 @@ resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
   vpc_zone_identifier  = data.aws_subnet_ids.default.ids
 
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
   min_size = 2
   max_size = 10
 
@@ -44,6 +49,91 @@ resource "aws_autoscaling_group" "example" {
     propagate_at_launch = true
   }
 }
+
+# LoadBalancer related resources
+
+resource "aws_lb" "example" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnet_ids.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  # By default, return a simple 404 page
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  # Allow inbound HTTP requests
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow request from all IPs
+  }
+
+  # Allow all outbound requests
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+# Tie the other resources together with a listener rule that sends
+# requests that match any path to the target group that contains your ASG (AutoScalingGroup of instances)
+
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+## DATASOURCES
 
 # Use a datasource to get read-only info from the provider, aws_vpc in this case. 
 data "aws_vpc" "default" {
@@ -55,13 +145,23 @@ data "aws_subnet_ids" "default" {
   vpc_id = data.aws_vpc.default.id
 }
 
+## VARIABLES
+
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
   type        = number
   default     = 8080
 }
 
-output "public_ip" {
-  value = aws_instance.example.public_ip
-  description = "The public IP address of the web server"
+## OUTPUT
+
+# output "public_ip" {
+#  value = aws_instance.example.public_ip
+#  description = "The public IP address of the web server"
+#}
+
+# Replaces the old public_ip from when we had just one instance
+output "alb_dns_name" {
+  value       = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
 }
