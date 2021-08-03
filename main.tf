94b65ceafcb83e0c3059de304eb6495849ad57fa
JERH O'CONNOR <JEROCONO@ie.ibm.com>
2021-08-03 18:30:12 +0100

    Add Auto Scaling group

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0943382e114f188e8"
+resource "aws_launch_configuration" "example" {
+  image_id      = "ami-0943382e114f188e8"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
 
@@ -13,8 +13,10 @@ resource "aws_instance" "example" {
   nohup busybox httpd -f -p ${var.server_port} &
   EOF
 
-  tags = {
-    Name = "terraform-example"
+  # Required when using a launch configuration with an auto scaling group.
+  # https://www.terraform.io/docs/providers/aws/r/launch_configuration.html
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
@@ -29,6 +31,30 @@ resource "aws_security_group" "instance" {
   }
 }
 
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+# Use a datasource to get read-only info from the provider, aws_vpc in this case. 
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Use another datasource that uses a datasource reference, to aws_vpc named default in this case,
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
   type        = number
@@ -36,6 +62,6 @@ variable "server_port" {
 }
 
 output "public_ip" {
-  value = "${aws_instance.example.public_ip}"
+  value = aws_instance.example.public_ip
   description = "The public IP address of the web server"
 }
