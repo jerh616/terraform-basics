a9604e5dab70563e6006f668007c0ba3fa293308
JERH O'CONNOR <JEROCONO@ie.ibm.com>
2021-08-03 17:33:26 +0100

    Simple webserver

--- main.tf
+++ main.tf
@@ -6,6 +6,12 @@ resource "aws_instance" "example" {
   ami           = "ami-02b4e72b17337d6c1"
   instance_type = "t2.micro"
 
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Johnnie 5 alive?" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
   tags = {
     Name = "terraform-example"
   }
